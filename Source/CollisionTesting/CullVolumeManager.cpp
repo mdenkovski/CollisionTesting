daa6c591b065a2dedd89cb09b0da4e01c5b48f94
Michael Denkovski <michaeldenkovski@hotmail.com>
2021-02-19 18:21:03 -0500

    dynamic render distance setting and updated cull changing method

--- Source/CollisionTesting/CullVolumeManager.cpp
+++ Source/CollisionTesting/CullVolumeManager.cpp
@@ -9,10 +9,9 @@
 ACullVolumeManager::ACullVolumeManager()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-	b_cullVolumeModified = false;
+	PrimaryActorTick.bCanEverTick = false;
+	//b_cullVolumeModified = false;
 
-	
 }
 
 // Called when the game starts or when spawned
@@ -22,76 +21,88 @@ void ACullVolumeManager::BeginPlay()
 	
 }
 
-// Called every frame
-void ACullVolumeManager::Tick(float DeltaTime)
+//// Called every frame
+//void ACullVolumeManager::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+
+//void ACullVolumeManager::LowerCullDistances()
+//{
+//	if (cullVolume == nullptr) return; //only proceed if we have a valid cull volume
+//
+//	if (!b_cullVolumeModified)
+//	{
+//		
+//			for (int i = 0; i < cullVolume->CullDistances.Num(); i++) //iterate through each cull distance pair
+//			{
+//				if (cullVolume->CullDistances[i].CullDistance != 0.0f) //check that the distance is one we want to halve
+//				{
+//					cullVolume->CullDistances[i].CullDistance = cullVolume->CullDistances[i].CullDistance / cullChangeFactor; //halve the value of each cull distance
+//					float distance = cullVolume->CullDistances[i].CullDistance;
+//					GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, FString::Printf(TEXT("Cull Distance changed to: %f"), distance));
+//				}
+//			}
+//		
+//		b_cullVolumeModified = true;
+//		GWorld->UpdateCullDistanceVolumes();
+//	}
+//}
+//
+//void ACullVolumeManager::RaiseCullDistances()
+//{
+//	if (cullVolume == nullptr) return; //only proceed if we have a valid cull volume
+//
+//	if (b_cullVolumeModified)
+//	{
+//		
+//			for (int i = 0; i < cullVolume->CullDistances.Num(); i++) //iterate through each cull distance pair
+//			{
+//				if (cullVolume->CullDistances[i].CullDistance != 0.0f) //check that the distance is one we want to halve
+//				{
+//					cullVolume->CullDistances[i].CullDistance = cullVolume->CullDistances[i].CullDistance * cullChangeFactor; // double the value of each cull distance back to normal
+//				}
+//			}
+//		
+//		b_cullVolumeModified = false;
+//		GWorld->UpdateCullDistanceVolumes();
+//	}
+//}
+//
+//
+////modify cull distance values that do not have a 0 value for distance
+//void ACullVolumeManager::SetCullDistances(float newDistance)
+//{
+//	if (cullVolume == nullptr) return; //only proceed if we have a valid cull volume
+//
+//	if (newDistance <= 0.0f)
+//	{
+//		return; //make sure there is a valid distance being applied
+//	}
+//
+//	
+//	for (int i = 0; i < cullVolume->CullDistances.Num(); i++) //iterate through each cull distance pair
+//	{
+//		if (cullVolume->CullDistances[i].CullDistance != 0.0f) //check that the distance is one we want to modify
+//		{
+//			cullVolume->CullDistances[i].CullDistance = newDistance; // set the cull distance to the new distance
+//		}
+//	}
+//	
+//	b_cullVolumeModified = false; // we are setting the new base value for the cull distance so this is an unmodified value
+//	GWorld->UpdateCullDistanceVolumes();
+//}
+
+void ACullVolumeManager::UpdateViewDistance(int QualityLevel)
 {
-	Super::Tick(DeltaTime);
-
-}
-
-void ACullVolumeManager::LowerCullDistances()
-{
-	if (cullVolume == nullptr) return; //only proceed if we have a valid cull volume
-
-	if (!b_cullVolumeModified)
-	{
-		
-			for (int i = 0; i < cullVolume->CullDistances.Num(); i++) //iterate through each cull distance pair
-			{
-				if (cullVolume->CullDistances[i].CullDistance != 0.0f) //check that the distance is one we want to halve
-				{
-					cullVolume->CullDistances[i].CullDistance = cullVolume->CullDistances[i].CullDistance / cullChangeFactor; //halve the value of each cull distance
-					float distance = cullVolume->CullDistances[i].CullDistance;
-					GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, FString::Printf(TEXT("Cull Distance changed to: %f"), distance));
-				}
-			}
-		
-		b_cullVolumeModified = true;
-		GWorld->UpdateCullDistanceVolumes();
-	}
-}
-
-void ACullVolumeManager::RaiseCullDistances()
-{
-	if (cullVolume == nullptr) return; //only proceed if we have a valid cull volume
-
-	if (b_cullVolumeModified)
-	{
-		
-			for (int i = 0; i < cullVolume->CullDistances.Num(); i++) //iterate through each cull distance pair
-			{
-				if (cullVolume->CullDistances[i].CullDistance != 0.0f) //check that the distance is one we want to halve
-				{
-					cullVolume->CullDistances[i].CullDistance = cullVolume->CullDistances[i].CullDistance * cullChangeFactor; // double the value of each cull distance back to normal
-				}
-			}
-		
-		b_cullVolumeModified = false;
-		GWorld->UpdateCullDistanceVolumes();
-	}
-}
-
-
-//modify cull distance values that do not have a 0 value for distance
-void ACullVolumeManager::SetCullDistances(float newDistance)
-{
-	if (cullVolume == nullptr) return; //only proceed if we have a valid cull volume
-
-	if (newDistance <= 0.0f)
-	{
-		return; //make sure there is a valid distance being applied
-	}
-
+	if (QualityLevel < 0 || QualityLevel >= 4) return; //make sure the Quality level is within applicable bounds
 	
-	for (int i = 0; i < cullVolume->CullDistances.Num(); i++) //iterate through each cull distance pair
-	{
-		if (cullVolume->CullDistances[i].CullDistance != 0.0f) //check that the distance is one we want to modify
-		{
-			cullVolume->CullDistances[i].CullDistance = newDistance; // set the cull distance to the new distance
-		}
-	}
-	
-	b_cullVolumeModified = false; // we are setting the new base value for the cull distance so this is an unmodified value
-	GWorld->UpdateCullDistanceVolumes();
+	UGameUserSettings* GameSettings = UGameUserSettings::GetGameUserSettings();
+
+	GameSettings->SetViewDistanceQuality(QualityLevel);
+	GameSettings->ApplySettings(false);
+
+
 }
 
--- Source/CollisionTesting/CullVolumeManager.h
+++ Source/CollisionTesting/CullVolumeManager.h
@@ -4,7 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Engine/CullDistanceVolume.h"
+//#include "Engine/CullDistanceVolume.h"
+#include "GameFramework/GameUserSettings.h"
 #include "CullVolumeManager.generated.h"
 
 UCLASS()
@@ -12,20 +13,17 @@ class COLLISIONTESTING_API ACullVolumeManager : public AActor
 {
 	GENERATED_BODY()
 
-		
-	bool b_cullVolumeModified;
+	//bool b_cullVolumeModified;
 
-	
-	
 public:	
 	// Sets default values for this actor's properties
 	ACullVolumeManager();
 
-	UPROPERTY(EditAnywhere)
+	/*UPROPERTY(EditAnywhere)
 	ACullDistanceVolume* cullVolume;
 
 	UPROPERTY(EditAnywhere)
-		float cullChangeFactor = 2.0f;
+		float cullChangeFactor = 2.0f;*/
 
 
 protected:
@@ -34,15 +32,18 @@ protected:
 
 public:	
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	//virtual void Tick(float DeltaTime) override;
 
-	UFUNCTION(BlueprintCallable)
+	/*UFUNCTION(BlueprintCallable)
 		void LowerCullDistances();
 
 	UFUNCTION(BlueprintCallable)
 		void RaiseCullDistances();
 
 	UFUNCTION(BlueprintCallable)
-		void SetCullDistances(float newDistance);
+		void SetCullDistances(float newDistance);*/
+
+	UFUNCTION(BlueprintCallable)
+		void UpdateViewDistance(int QualityLevel);
 
 };
